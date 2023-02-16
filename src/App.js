9202862e44fa32a15755ee5fa7d17acb32f94bad
hamza <arastiranhamza@gmail.com>
2023-02-16 18:24:55 +0300

    add router and clear page folder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useTranslation } from "react-i18next";
+import { Routes, Route, BrowserRouter, useLocation } from "react-router-dom";
+import AboutMe from "./pages/AboutMe";
+import Home from "./pages/Home";
+import Router from "./pages/router";
+
 function App() {
   const { t, i18n } = useTranslation();
   const clickHandle = async (lang) => {
     await i18n.changeLanguage(lang);
   };
   return (
-    <div className="App">
-      Aktif dil: {i18n.language} <br />
-      <button onClick={() => clickHandle("tr")}>Tr</button>
-      <button onClick={() => clickHandle("en")}>En</button>
-      <br />
-      <h3>{t(`welcome`)}</h3>
-    </div>
+    // <div className="App">
+    //   Aktif dil: {i18n.language} <br />
+    //   <button onClick={() => clickHandle("tr")}>Tr</button>
+    //   <button onClick={() => clickHandle("en")}>En</button>
+    //   <br />
+    //   <h3>{t(`welcome`)}</h3>
+    // </div>
+    <Router />
   );
 }
 
---
+++ src/pages/AboutMe/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Styles.AboutMe.css";
+
+const AboutMe = () => {
+  return <div>AboutMe</div>;
+};
+
+export default AboutMe;
